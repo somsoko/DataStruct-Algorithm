8f8997a05d84e78232f11618af2adac468d52e7d
somsoko <somko4758@naver.com>
2026-08-04 14:26:27 +0900

    {정수 키, 단일 정수 데이터}를 원소로 가지도록 최대 힙 클래스 수정 우선순위 큐로 사용 가능

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,13 +1,24 @@
 public class MaxHeap {
     // 힙은 완전 이진트리이기 때문에 배열로 구현
-    private int[] heap;
+    private Element[] heap;
 
     // 현재 힙의 원소 갯수
     private int n = 0;
 
+    // 정수 key와 단일 정수 data를 가지는 Element
+    public class Element {
+        int key;
+        int data;
+
+        public Element(int key, int data) {
+            this.key = key;
+            this.data = data;
+        }
+    }
+
     public MaxHeap(int size) {
         // 0번은 사용하지 않기 때문에 size+1
-        heap = new int[size+1];
+        heap = new Element[size+1];
     }
 
     public boolean heapFull() {
@@ -19,14 +30,14 @@ public class MaxHeap {
     }
 
     /** maxheap에 아이템 삽입 */
-    public void insert(int item) {
+    public void insert(Element item) {
         if(heapFull()) {
             System.out.println("!full!");
             return;
         }
 
         int i = ++n;
-        while((i != 1) && (item > heap[i/2])) {
+        while((i != 1) && (item.key > heap[i/2].key)) {
             heap[i] = heap[i/2];
             i /= 2;
         }
@@ -34,23 +45,25 @@ public class MaxHeap {
     }
 
     /** maxheap의 루트 삭제하고 리턴 */
-    public int delete() {
+    public Element delete() {
         if(heapEmpty()) {
             System.out.println("!empty!");
-            return -1;
+            return null;
         }
 
-        int item = heap[1];
-        int temp = heap[n--];
+        Element item = heap[1];
+        Element temp = heap[n];
+        heap[n] = null;
+        n--;
 
         int parent = 1;
         int child = 2;
         while(child <= n) {
             // 두 개의 자식 중 큰 쪽과 비교
-            if((child < n) && (heap[child] < heap[child +1])) {
+            if((child < n) && (heap[child].key < heap[child +1].key)) {
                 child++;
             }
-            if(temp > heap[child]) break;
+            if(temp.key > heap[child].key) break;
             heap[parent] = heap[child];
             parent = child;
             child *= 2;
@@ -60,31 +73,32 @@ public class MaxHeap {
         return item;
     }
 
-    // maxheap 출력
+    // maxheap의 element의 data 출력
     public void show() {
         for(int i = 1; i <= n; i++)
-            System.out.print(heap[i]+" ");
+            System.out.print(heap[i].data+" ");
 
         System.out.println();
     }
 
 
     public static void main(String[] args) {
-        MaxHeap mh = new MaxHeap(6);
+        // priority queue
+        MaxHeap pq = new MaxHeap(6);
 
-        mh.insert(14);
-        mh.insert(15);
-        mh.insert(10);
-        mh.insert(20);
-        mh.insert(2);
-        mh.insert(5);
+        pq.insert(pq.new Element(14,14));
+        pq.insert(pq.new Element(15,15));
+        pq.insert(pq.new Element(10,10));
+        pq.insert(pq.new Element(20,20));
+        pq.insert(pq.new Element(2,2));
+        pq.insert(pq.new Element(5,5));
 
-        mh.insert(21);
-        mh.show();
+        pq.insert(pq.new Element(21,21));
+        pq.show();
 
-        System.out.println(mh.delete());
-        mh.show();
-        System.out.println(mh.delete());
-        mh.show();
+        System.out.println(pq.delete().data);
+        pq.show();
+        System.out.println(pq.delete().data);
+        pq.show();
     }
 }
